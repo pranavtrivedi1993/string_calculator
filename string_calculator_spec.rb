12acb713eb9fbd6ad4d742fbc42c2b2460de4962
Pranav Trivedi <73770397+pranavtrivedi1993@users.noreply.github.com>
2025-06-01 12:04:51 +0530

    Rspec test cases for string calculator

---
+++ string_calculator_spec.rb
@@ -0,0 +1,18 @@
+# string_calculator_spec.rb
+require './string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '#add' do
+    it 'returns 0 for empty string' do
+      expect(subject.add("")).to eq(0)
+    end
+
+    it 'returns number itself for one number' do
+      expect(subject.add("1")).to eq(1)
+    end
+
+    it 'returns sum for two numbers' do
+      expect(subject.add("1,2")).to eq(3)
+    end
+  end
+end
