a2cd7add8388e691bf1fa836e5a5a5ecf7c93907
Pranav Trivedi <73770397+pranavtrivedi1993@users.noreply.github.com>
2025-06-01 12:57:03 +0530

    Raise exception for negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,19 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    
+  
     delimiter = ","
     if numbers.start_with?("//")
       delimiter_spec, numbers = numbers.split("\n", 2)
       delimiter = delimiter_spec[2]
     end
-    
-    numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
+  
+    numbers = numbers.gsub("\n", delimiter)
+    nums = numbers.split(delimiter).map(&:to_i)
+  
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+  
+    nums.sum
   end
 end
